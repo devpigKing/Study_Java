23db0a144aed3be880ce48b06c080f192aa9fa55
jwhydh <devpigking@gmail.com>
2023-07-10 15:13:27 +0900

    #feat : 자바 공부를 위한 환경 설정과 첫번째 예시 코드입니다.

---
+++ src/HelloJava.java
@@ -0,0 +1,19 @@
+
+// 1. 자바의 모든 코드는 클래스(인터페이스, enum 등 포함) 단위로 작성 및 구성
+// 하나의 파일에 하나의 클래스 작성 + 클래스 이름과 파일 이름 일치하도록 작성
+public class HelloJava {
+
+    // 2. 자바 프로그램은 public static void main에서 시작
+    public static  void main(String[] args) {
+
+        // 3. 대소문자 구분 : system과 System은 다른 이름이다.
+        // 4. 문장의 끝은 ; 로 표시한다(필수)
+        // 5. 문자 리터럴 :
+        //    문자열 : 0개 이상의 문자 집합, "" 로 표시
+        //    문자 : 1개의 문자 : ' '로 표시
+        System.out.println("Hello, Java with Eclipse!!!");
+        System.out.println("First Program");
+        System.out.println("Bye ~~~~~");
+        System.out.println('C');
+    }
+}
