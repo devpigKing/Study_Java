b2ee09d473c3e7fea25c9dbdcc4914ae846ceb07
jwhydh <devpigking@gmail.com>
2023-07-11 14:23:15 +0900

    #feat : BreakAndContinue.java, 독특한 break, continue 기본 코드입니다.(label문)

--- src/Ex03_ControlStatement/BreakAndContinue.java
+++ src/Ex03_ControlStatement/BreakAndContinue.java
@@ -3,13 +3,14 @@ package Ex03_ControlStatement;
 public class BreakAndContinue {
     public static void main(String[] args) {
 
+        // outer는 for문에 대한 이름 : label문 이라고 한다.
         outer: for (int i = 0; i < 10; i++) {
             System.out.printf("[i %d]\n", (i+1));
             inner: for (int j = 0; j < 10; j++) {
                 System.out.printf("[j = %d]", (j+1));
 
                 if( i == 5) {
-                    break outer;
+                    break outer; // outer 이름이 붙은 반복문 or switch문 중단.
                 }
             }
 
