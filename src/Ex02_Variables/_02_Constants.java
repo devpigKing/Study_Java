2721262450dc5af66b423d996c697d28df0efb50
jwhydh <devpigking@gmail.com>
2023-07-11 10:39:16 +0900

    #feat : _02_Constants.java, 상수 선언에 관한 기본 코드입니다.

---
+++ src/Ex02_Variables/_02_Constants.java
@@ -0,0 +1,10 @@
+package Ex02_Variables;
+
+public class _02_Constants {
+    public static void main(String[] args) {
+        // 상수를 저장해보자
+        // 상수 이름은 대문자로 표시한다.
+        final String DATE_OF_MYBIRTH = "1996-02-16";
+        System.out.println(DATE_OF_MYBIRTH);
+    }
+}
