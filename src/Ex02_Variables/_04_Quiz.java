638698fdeb7e0c1c3d1a03cf4da7f78731276fae
jwhydh <devpigking@gmail.com>
2023-07-11 11:29:38 +0900

    #feat : _04_Quiz.java, 자바 기본 자료형에 대한 간단한 퀴즈 예시입니다.

---
+++ src/Ex02_Variables/_04_Quiz.java
@@ -0,0 +1,21 @@
+package Ex02_Variables;
+
+/*
+(실행 결과)
+양천03번 버스
+약 4분 후 도착
+남은 거리 1.5km
+ */
+public class _04_Quiz {
+    public static void main(String[] args) {
+        String busNo = "양천03";
+
+        int minute = 4;
+
+        double distance = 1.5;
+
+        System.out.println(busNo + "번 버스");
+        System.out.println("약 " + minute + "분 후 도착");
+        System.out.println("남은 거리 " + distance + "km");
+    }
+}
