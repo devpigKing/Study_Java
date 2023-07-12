b1738f6bfdac8353d8c0afb80d9f55c35fdd9700
jwhydh <devpigking@gmail.com>
2023-07-12 12:25:47 +0900

    #feat : 다차원 배열에 대한 기본 사용 코드입니다.

--- src/Ex04_Array/_02_MultiArray.java
+++ src/Ex04_Array/_02_MultiArray.java
@@ -2,6 +2,19 @@ package Ex04_Array;
 
 public class _02_MultiArray {
     public static void main(String[] args) {
+        // 4. 2차원 배열 (배열의 요소가 다른 배열인 경우)
+        int[][] arr = new int[5][7];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 7; j++) {
+                arr[i][j] = (int)(Math.random()*90)+10;
+            }
+        }
 
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 7; j++) {
+                System.out.printf("[%d]", arr[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
