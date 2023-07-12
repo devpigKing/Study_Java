e216f76558d4dfbeee363728098edbc4c272dcf8
jwhydh <devpigking@gmail.com>
2023-07-12 15:45:57 +0900

    #feat : 다차원 배열에 대한 기본 사용 코드입니다.

--- src/Ex04_Array/_02_MultiArray.java
+++ src/Ex04_Array/_02_MultiArray.java
@@ -16,5 +16,9 @@ public class _02_MultiArray {
             }
             System.out.println();
         }
+
+        // 5. 2차원 배열 초기화 : {}를 중첩해서 각 배열의 초기 값 지정
+        int[][] arr2 = new int[][] {{1,2}, {3,4}, {5,6}};
+
     }
 }
