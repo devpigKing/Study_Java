e680da62a7a36a7cd9034c43609b51155984481d
jwhydh <devpigking@gmail.com>
2023-07-13 10:27:03 +0900

    #refactor : _00_MethodDemo.java, 20*10 크기의 박스 그리는 함수를 통한 메서드 기본 사용법 코드입니다.

--- src/Ex05_Method/_00_MethodDemo.java
+++ src/Ex05_Method/_00_MethodDemo.java
@@ -2,8 +2,8 @@ package Ex05_Method;
 
 public class _00_MethodDemo {
 
-    public static void drawBox() {
-        // 20 x 10 크기의 박스 그리기
+    // 메서드 정의 (만들기)
+    private static void drawBox() {
 
         for (int row = 0; row < 10; row++) {
             for (int col = 0; col < 20; col++) {
@@ -16,10 +16,9 @@ public class _00_MethodDemo {
             System.out.println();
         }
     }
-
     public static void main(String[] args) {
 
-        drawBox();
+        drawBox(); // 매서드 실행
 
     }
 }
