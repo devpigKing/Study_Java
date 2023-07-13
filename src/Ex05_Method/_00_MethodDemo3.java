9625844fa1eb2545ffafa8a53020d3ff703d8a89
jwhydh <devpigking@gmail.com>
2023-07-13 11:20:53 +0900

    #refactor : _00_MethodDemo3.java, 두 정수를 더하는 메서드 만들기 입니다.(여러개의 매개변수를 입력 받아 return하는 메서드,)

---
+++ src/Ex05_Method/_00_MethodDemo3.java
@@ -0,0 +1,15 @@
+package Ex05_Method;
+
+public class _00_MethodDemo3 {
+
+    // 두 정수를 더하는 함수 만들기
+    public static int sum(int a, int b) {
+        int result = a + b;
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(sum(99, 99));
+    }
+}
