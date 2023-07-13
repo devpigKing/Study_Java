83d95d2952064d10ad667ccb56fef49b73fcc28f
jwhydh <devpigking@gmail.com>
2023-07-13 13:48:37 +0900

    #refactor : _00_MethodDemo4.java, 매개변수로 참조 자료형을 받는 메서드에 대한 예시 코드입니다.

--- src/Ex05_Method/_00_MethodDemo4.java
+++ src/Ex05_Method/_00_MethodDemo4.java
@@ -10,9 +10,21 @@ public class _00_MethodDemo4 {
         }
     }
 
+    public static int sum(int...values) {
+        int result = 0;
+        for (int v : values) {
+            result += v;
+        }
+        return result;
+    }
+
+    // 불특정 개수의 전달인자를 받는 배열 전달인자 변수
+
     public static void main(String[] args) {
         int[] ar = {1, 2, 3};
         useArrayParam(ar);
         useArrayParam(new int[] {1,2,3,4});
+
+        System.out.println(sum(1,2,3,4,5,6,7,8,9));
     }
 }
