83072b46e61f71150bd0c525c1da5d704b26cbd3
jwhydh <devpigking@gmail.com>
2023-07-17 17:18:14 +0900

    #refactor : ToDo.java, ToDoApp.java, 클래스를 적용한 간단한 ToDo_List 프로그램 코드입니다.

--- src/Ex07_ToDo/ToDoApp.java
+++ src/Ex07_ToDo/ToDoApp.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 
 public class ToDoApp {
 
-    private Scanner sc = new Scanner(System.in);
-    private ToDo[] toDos = new ToDo[1000]; // 할 일을 저장, 관리하는 배열
+    private final Scanner sc = new Scanner(System.in);
+    private final ToDo[] toDos = new ToDo[1000]; // 할 일을 저장, 관리하는 배열
     private int nextPosition = 0; // 다음 할 일을 등록할 때, 저장할 배열의 위치 값
 
     /**
@@ -18,23 +18,9 @@ public class ToDoApp {
             String select = selectTask();
             switch (select) {
                 case "1": // 등록 처리
-                    // 1. ToDo 인스턴스 만들기
-                    ToDo toDo = new ToDo();
+                    ToDo toDo = inputNewToDo();
 
-                    // 2. 사용자 입력 + ToDo 인스턴스에 저장
-                    System.out.print("할 일 제목 : ");
-                    String title = sc.nextLine();
-
-                    System.out.print("할 일 내용 : ");
-                    String content = sc.nextLine();
-                    Date now = new Date();
-
-                    // 입력 데이터를 toDo 인스턴스에 저장
-                    toDo.setTitle(title);
-                    toDo.setContent(content);
-                    toDo.setRegDate(now);
-
-                    // 3. ToDo 인스턴스를 할 일 관리 배열에 추가
+                    // 4. ToDo 인스턴스를 할 일 관리 배열에 추가
                     toDos[nextPosition] = toDo;
                     nextPosition++; // 다음에 등록할 위치 변경
 
@@ -42,12 +28,8 @@ public class ToDoApp {
                 case "2":
                     if ( nextPosition == 0 ) {
                         System.out.println("등록된 할 일이 없습니다.");
-                        break;
-                    }
-                    for (int i = 0; i < nextPosition; i++) {
-                        // 처음부터 마지막 할 일이 등록되어 있는 위치까지 순회
-                        String info = toDos[i].info();
-                        System.out.println(info);
+                    } else {
+                        showAllToDos();
                     }
                     break;
                 case "3": break;
@@ -77,6 +59,36 @@ public class ToDoApp {
         String select = sc.nextLine();
 
         return select;
+    }
+
+    private ToDo inputNewToDo() {
+        // 1. ToDo 인스턴스 만들기
+        ToDo toDo = new ToDo();
+
+        // 2. 사용자 입력 + ToDo 인스턴스에 저장
+        System.out.print("할 일 제목 : ");
+        String title = sc.nextLine();
+
+        System.out.print("할 일 내용 : ");
+        String content = sc.nextLine();
+        Date now = new Date();
+
+        // 3. 입력 데이터를 toDo 인스턴스에 저장
+        toDo.setTitle(title);
+        toDo.setContent(content);
+        toDo.setRegDate(now);
+
+        return toDo;
+    }
+
+    private void showAllToDos() {
+
+        for (int i = 0; i < nextPosition; i++) {
+            System.out.println("[전체 할 일 목록]");
+            // 처음부터 마지막 할 일이 등록되어 있는 위치까지 순회
+            String info = toDos[i].info();
+            System.out.println(info);
+        }
     }
 
     public static void main(String[] args) {
