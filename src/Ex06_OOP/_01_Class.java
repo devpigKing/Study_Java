0e87183e900cdd264108e2ba853a631b41058a82
jwhydh <devpigking@gmail.com>
2023-07-18 16:44:26 +0900

    #feat : 객체지향의 시작입니다.

---
+++ src/Ex06_OOP/_01_Class.java
@@ -0,0 +1,12 @@
+package Ex06_OOP;
+
+public class _01_Class {
+    public static void main(String[] args) {
+        // 객체 지향 프로그래밍 (OOP : Object-Oriented Programming)
+        // 유지보수 용이
+        // 높은 재사용성
+
+
+
+    }
+}
