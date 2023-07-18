0fa0255b6279f905a2cf9ce87f5a7f68c839d1e3
jwhydh <devpigking@gmail.com>
2023-07-18 16:43:53 +0900

    #feat : _05_For.java, 반복문 연습 코드입니다.

---
+++ src/Ex03_ControlStatement/_05_For.java
@@ -0,0 +1,14 @@
+package Ex03_ControlStatement;
+
+public class _05_For {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 10; i++) {
+            System.out.println("반복문 연습 " + i);
+        }
+
+        for(int j = 0; j < 10; j += 2) {
+            System.out.println(j);
+        }
+    }
+}
