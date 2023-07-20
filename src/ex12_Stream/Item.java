a9a15eec46225342981cc7d61280c35c532c9cb1
jwhydh <devpigking@gmail.com>
2023-07-20 14:57:26 +0900

    #feat : Item 도메인입니다.

---
+++ src/ex12_Stream/Item.java
@@ -0,0 +1,30 @@
+package ex12_Stream;
+
+import java.io.Serializable;
+
+public class Item implements Serializable { // io에 사용할 클래스는 Serializable 인터페이스를 구현해야 한다.
+
+    private int no;
+    private String name;
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d][%s]", no, name);
+    }
+}
