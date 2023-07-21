d2d58bd9734396595c049e7e3eff282071457e86
jwhydh <devpigking@gmail.com>
2023-07-21 17:19:40 +0900

    #refactor : BizCardApp.java, BizCard.java 명함 프로그램 코드입니다.

--- src/ex12_Stream/BizCard.java
+++ src/ex12_Stream/BizCard.java
@@ -1,98 +1,84 @@
 package ex12_Stream;
 
-public class BizCard {
+import java.io.Serializable;
+
+public class BizCard implements Serializable {
 
     // 1. 필드
-    private int no;    // 관리 번호 (자동 증가 번호)
-    private String name;   // 이름
-    private String company;  // 회사이름
-    private String dept;  // 부서 이름
-    private String title; // 직함
-    private String email; // 이메일
-    private String phone; // 전화번호 (휴대전화)　
+    private int no;				// 관리 번호 ( 자동 증가 번호 )
+    private String name;		// 이름
+    private String company;		// 회사이름
+    private String dept;		// 부서이름
+    private String title;		// 직함
+    private String email;		// 이메일
+    private String phone;		// 전화번호 (휴대전화)
 
-
-    // 생성자
-    public BizCard() {
-    }
-
+    // 2. 생성자 (constructor)
+    public BizCard() {} // 전달인자 없는 생성자 메서드를 만들면 특별한 이유가 없는 한 전달인자 없는 생성자도 만듭니다.
     public BizCard(int no, String name, String phone) {
         this.no = no;
         this.name = name;
         this.phone = phone;
     }
 
-
-    // getter&setter
+    // 3. getter, setter
     public int getNo() {
         return no;
     }
-
     public void setNo(int no) {
         this.no = no;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getCompany() {
         return company;
     }
-
     public void setCompany(String company) {
         this.company = company;
     }
-
     public String getDept() {
         return dept;
     }
-
     public void setDept(String dept) {
         this.dept = dept;
     }
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getPhone() {
         return phone;
     }
-
     public void setPhone(String phone) {
         this.phone = phone;
     }
 
-    // toString 메서드 재정의
-    // 객체의 정보를 간단한 문자열로 반환하는 Object 클래스의 메서드
+    // 4. toString 메서드 재정의
+    //    객체의 정보를 간단한 문자열로 반환하는 Object 클래스의 메서드
     @Override
     public String toString() {
-        // StringBuilder 문자열 여러개를 결합한다.
         StringBuilder sb = new StringBuilder(100);
-        sb.append(String.format("[%4s] : %d","번호", no))
-                .append(String.format("[%4s] : %d\n", "이름", name))
-                .append(String.format("[%4s] : %d\n", "회사", company))
-                .append(String.format("[%4s] : %d\n", "부서", dept))
-                .append(String.format("[%4s] : %d\n", "직함", title))
-                .append(String.format("[%4s] : %d\n", "이메일", email))
-                .append(String.format("[%4s] : %d\n", "전화번호", phone));
+        sb.append(String.format("[%4s] : %d\n", "번호", no))
+                .append(String.format("[%4s] : %s\n", "이름", name))
+                .append(String.format("[%4s] : %s\n", "회사", company))
+                .append(String.format("[%4s] : %s\n", "부서", dept))
+                .append(String.format("[%4s] : %s\n", "직함", title))
+                .append(String.format("[%4s] : %s\n", "이메일", email))
+                .append(String.format("[%4s] : %s\n", "전화번호", phone));
         return sb.toString();
+
     }
+
 }
--- src/ex12_Stream/BizCardApp.java
+++ src/ex12_Stream/BizCardApp.java
@@ -1,5 +1,6 @@
 package ex12_Stream;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -8,7 +9,23 @@ public class BizCardApp {
     private int nextBizCardNo = 1;
     private ArrayList<BizCard> bizCards;
     public BizCardApp() {
-        bizCards = new ArrayList<BizCard>();
+//        bizCards = new ArrayList<BizCard>();
+        FileInputStream fis = null;  // 파일에서 읽는 객체
+        ObjectInputStream ois = null;  // byte[] -> 객체 변환하는 객체
+        try {
+            fis = new FileInputStream("bizcards.dat");
+            ois = new ObjectInputStream(fis);
+            bizCards = (ArrayList<BizCard>)ois.readObject();
+            if (bizCards.size() > 0) { // 읽어 온 목록에 데이터가 1개 이상인 경우에만 다음 번호 조정
+                BizCard lastBizCard = bizCards.get(bizCards.size() - 1); // 마지막 할 일
+                nextBizCardNo = lastBizCard.getNo() + 1; // 마지막 할 일 번호의 다음 번호를 다음에 생성할 ToDo의 번호로 저장
+            }
+        } catch (IOException | ClassNotFoundException ex) { // 두 종류의 예외를 한 곳에서 처리
+            bizCards = new ArrayList<>(); // 파일 읽기 실패하면 빈 ArrayList로 초기화
+        } finally {
+            try { ois.close(); } catch (Exception ex) { /* do nothin - ignore exception */ }
+            try { fis.close(); } catch (Exception ex) { /* do nothin - ignore exception */ }
+        }
     }
 
     public void doManage() {
@@ -29,13 +46,29 @@ public class BizCardApp {
                         showMember();
                     }
                     break;
-                case "3": // 검색
+                case "3":// 검색
+                    System.out.println("검색어를 입력하세요 : ");
+                    String searchName = sc.nextLine();
+
+                    ArrayList<BizCard> searchedCards = new ArrayList<BizCard>();
+
+                    for( BizCard bizCard2 : bizCards) {
+                        if (bizCard2.getName().contains(searchName)) {
+                            searchedCards.add(bizCard2);
+                        }
+                    }
+
+                    for(BizCard searchCard: searchedCards) {
+                        System.out.println(searchCard);
+                    }
                     break;
                 case "4": // 수정
+                    System.out.println("수정할 대상을 입력하세요 : ");
                     break;
                 case "5": // 삭제
                     break;
                 case "6": // 저장
+                    saveBizCards();
                     break;
                 case "9": // 종료
                     System.out.println("명함 관리 프로그램을 종료합니다.");
@@ -65,10 +98,9 @@ public class BizCardApp {
     }
 
     private void showMember() {
+        System.out.println("[사원 정보 목록]");
         for ( BizCard s : bizCards) {
-            System.out.println("[사원 정보 목록]");
-            String sb = s.toString();
-            System.out.println(sb);
+            System.out.println(s.toString());
         }
     }
 
@@ -87,6 +119,24 @@ public class BizCardApp {
         return bizCard;
     }
 
+
+    private void saveBizCards() {
+        FileOutputStream fos = null;
+        ObjectOutputStream oos = null;
+        try {
+            fos = new FileOutputStream("bizcards.dat");	// 저장
+            oos = new ObjectOutputStream(fos);			// 객체 -> byte[] 변환
+            oos.writeObject(bizCards);  // 파일에 데이터 쓰기
+            System.out.println("파일에 할 일 데이터를 저장했습니다.");
+        } catch (IOException e) { // IOException은 FileNotFoundException의 부모 클래스
+            e.printStackTrace();  // 예외 정보를 화면에 출력, 테스트 할 때만 사용
+        } finally {
+            try { oos.close(); } catch (Exception ex) { /* do nothing - ignore exception */ }
+            try { fos.close(); } catch (Exception ex) { /* do nothing - ignore exception */ }
+        }
+    }
+
+    ////////////////////////////////
     public static void main(String[] args) {
         BizCardApp app = new BizCardApp();
          app.doManage();
